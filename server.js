62b0acd849363f72765a9686a6e49ce534e9718e
Jason Gardella <jason.a.gardella@gmail.com>
2016-09-23 13:24:10 -0400

    Remove unnecessary middleware.

--- server.js
+++ server.js
@@ -25,13 +25,6 @@ let github = new GitHubApi({
   },
   timeout: 5000 });
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "http://localhost:8080");
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
